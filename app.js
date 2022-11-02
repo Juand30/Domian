2491f29ffba65623a746ba5e83e5dad7d7885b5c
Juand30 <juandp2290@gmail.com>
2022-11-02 17:07:29 +0000

    Proyecto Domian

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let pronoun = ['the','our'];
+const random = pronoun[Math.floor(Math.random() * pronoun.length)];
+
+let adj = ['great', 'big' ];
+const random2 = pronoun[Math.floor(Math.random() * adj.length)];
+
+let noun = ['jogger','racoon'];
+const random3 = noun[Math.floor(Math.random() * noun.length)];
+
+let com = ['.com', '.es'];
+const random4 = com[Math.floor(Math.random() * com.length)];
+
+console.log(random+random2+random3+random4);
